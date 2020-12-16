9306280bc61b1e4070be6ab3df87867ea7475ac3
RDeJonghe <rdejonghe@hotmail.com>
2020-12-16 07:18:22 -0700

    12/16 Practice

--- functions.js
+++ functions.js
@@ -94,16 +94,129 @@ console.log(club()); */
 // from Udemy course
 // Dice Roll Function
 
-function dice () {
-  let num = Math.ceil(Math.random() * 6);
-  
+/*
+function rollDie () {
+  let roll = Math.ceil(Math.random() * 6);
+  console.log(`Rolled: ${roll}`);
 }
 
-function throwDice() {
-  (dice();
-  dice();
-  return 'nice throw'
-};
+function throwDice(numRolls) {
+  for(let i = 0; i < numRolls; i++) { // since i starts at 0 it is < numRolls
+    rollDie();                        // can also make it i = 1 and <= numRolls
+  }
+}
 
-console.log(throwDice());
+throwDice(6);
+*/
 
+// Note: with functions if a parameter is left blank when you call the function JS gives it a value
+// of undefined.
+
+/*
+function add(x, y) {
+  let result = x + y;
+  return result
+}
+
+console.log(add(3,2));
+*/
+
+// Can only return 1 thing from a function, only be one value (you can have multiple return statements
+// though when using if/else, but only one value is hit on and returned). Return statements end function execution
+
+// note for Boolean methods a standard naming convention uses "is" so for example function isPurple
+// you use "is" because it checks if it's true or not.
+
+// This is a boolean expression, this is shorter than an if/else statement and checks the same thing
+// This will check for true or false
+/*
+function isPurple(color) {
+  return color.toLowerCase() === 'purple' || color.toLowerCase() === 'lilac';
+}
+
+//console.log(isPurple('blue'));
+
+// can also use a function to loop over an array
+
+function containsPurple(arr) {
+  for(let element of arr) {
+    if(element === 'purple' || element === 'magenta') {
+      return true;
+    }// if there were an else after if, it would hit on the first run through and go to false, so return
+    // moved below
+  }
+  return false; // note where this return is, it's outside of the loop so it can loop through all elements
+}
+
+console.log(containsPurple(['blue', 'pink', 'purple']))
+*/
+
+
+// Example from Udemy course
+// Write a isValidPasswordFunction
+// accepts 2 arguments: password and username
+// Password must:
+// - be at least 8 characters
+// - cannot contain spaces
+// - cannot contain the username
+// If all requirements are met return true
+// otherwise return false
+
+// in this example he "reverses logic", easier syntax to check if any one of the things is false
+// did this instead of using && and checking if everything is true or nesting a bunch of if statements
+
+// note he uses multiple if statements instead of doing if/else
+// note checks for indexOf ' ' the space character. Checks if it = -1. If a space is not 
+// in the password it will evaluate to -1, that's what we want. So we check to see if it doesn't = that
+// with this it could also be done with setting variables and checking the variables
+
+function isValidPassword(password, username) {
+  if(password.length < 8) {
+    return false;
+  }
+  if(password.indexOf(' ') !== -1) {
+    return false;
+  }
+  /*
+  if(password.indexOf(username) !== -1) { // can use indexOf with multiple characters
+    return false;
+  }
+  */
+  if(password.includes(username)) { // this also works
+    return false;
+  }
+
+ return true;
+}
+
+// can also write like this with or statements, but still checking False instead of true
+function isValid(password, username) {
+  if(
+    password.length < 8 ||
+    password.indexOf(' ') !== -1 ||
+    password.includes(username)
+  ) {
+  return false;
+  }
+  return true;
+}
+
+// can also write with variables
+/*
+function Valid(password, username) {
+  const tooShort = password.length < 8;
+  const hasSpace = password.indexOf(' ') !== -1;
+  const tooSimilar = password.includes(username);
+  if(tooShort || hasSpace || tooSimilar) return false; // note {} not needed after if in this case
+  return true;
+}
+*/
+
+// can also switch this with not ! operator
+function valid(password, username) {
+  const tooShort = password.length < 8;
+  const hasSpace = password.indexOf(' ') !== -1;
+  const tooSimilar = password.includes(username);
+  if(!tooShort && !hasSpace && !tooSimilar) return true; // note && is needed
+  return false;
+}
