f4b2c9bd9b6206a72f52096711a819c7317df0d5
RDeJonghe <rdejonghe@hotmail.com>
2020-12-24 17:23:56 -0700

    Shortcuts practice

---
+++ shortcuts.js
@@ -0,0 +1,66 @@
+// Shortcuts practice
+
+// 1. ⌥↓ / ⌥↑ Move line up/down
+
+let str = 'Yo';
+let str2 = 'Whaddup';
+let str3 = 'Today?';
+
+// 2. ⇧⌥↓ / ⇧⌥↑ Copy Line up/down
+
+let str = 'Yo';
+let str2 = 'Whaddup';
+let str3 = 'Today?';
+
+// 3. ⌘X Cut line - delete entire line / This also seems to work the same ⇧⌘K
+
+let str = 'Yo';
+let str2 = 'Whaddup';
+let str3 = 'Today?';
+
+// 4. ⌘C copy line - can copy entire line, don't have to select it
+
+let str = 'Yo';
+let str2 = 'Whaddup';
+let str3 = 'Today?';
+
+// 5. ⌘Enter / ⇧⌘Enter - Insert line below or above
+
+let str = 'Yo';
+let str2 = 'Whaddup';
+let str3 = 'Today?';
+
+// 6. ⇧⌘\ - Jump to matching bracket
+
+if(str.length < 5) {
+  console.log('long');
+else {
+  console.log('short');
+}
+}
+
+// 7. ⌘] / ⌘[  Indent/outdent line (can do it from middle of line instead of using tab from beginning)
+
+let str = 'Yo';
+  let str2 = 'Whaddup';
+let str3 = 'Today?';
+
+// 8. Home / End - go to beginning or end of line. (on laptop this is fn and left or right arrow)
+
+let str = 'Yo';
+
+// 9. ⌘↑ / ⌘↓ Go to beginning / end of file
+
+// 10. ⌃PgUp / ⌃PgDn  Scroll line up / down (use instead of scrolling with mouse/track bar)
+
+
+
+// delete right - fn Delete
+// command shift - select entire line
+// option shift - select word
+
+// Home / End = fn left right arrow
+// PgUp / PgDn = fn down arrow, fn up arrow
+
+
+
